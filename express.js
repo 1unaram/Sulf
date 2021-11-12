ebc699deb904bf6da4b82bed9fcf4b4a0c6b155d
1unara <haram620@icloud.com>
2021-11-13 01:31:59 +0900

    node 및 nodemon 설정

--- express.js
+++ express.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.set('port', process.env.PORT || 8080);
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '/frontend_dev/build')));
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 서버 실행 중 ..')

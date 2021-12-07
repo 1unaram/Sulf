c234a9153982204222a51a32f271c220f2dac59f
1unara <haram620@icloud.com>
2021-12-08 00:27:19 +0900

    back merge 전 back 커밋

---
+++ frontend_dev/src/Components/Howtouse.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Howtouse = () => {
+    return (
+        <div>
+
+            <header>
+                <div class="item"><a href="#">사이트 이용 방법</a></div>
+            </header>
+
+            <section>
+                <div class="box">
+                    <span class="item">1. 설문조사를 부탁할 일이 생겼다!</span>
+                    <span class="item">2. Sulf에 들어와 설문 조사를 3개 이상 완료한다.</span>
+                    <span class="item">3. 설문 조사를 완료할 때마다 {콩}을 부여받는다.</span>
+                    <span class="item">4. 부여받은 {콩}으로 설문 대상과 기간을 포함하여 본인의 설문 조사를 올린다.</span>
+                    <span class="item">5. 설문 조사 걱정 끝~!</span>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default Howtouse
---
+++ frontend_dev/src/Components/Main.js
@@ -0,0 +1,74 @@
+/* 메인 페이지  */
+
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+// 컴포넌트 연결
+import './styles/Main.css';
+
+const Main = () => {
+    return (
+        <div className="main">
+
+            <header>
+                <div className="inner">
+
+                    <div className="logo">
+                        <h1><Link to="/">Sulf</Link></h1>
+                    </div>
+
+                    <ul className="util">
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/aboutus">About us</Link></li>
+                        <li><Link to="/service">Service</Link></li>
+                    </ul>
+
+                </div>
+            </header>
+
+            <figure>
+                <div className="inner">
+                    <p>
+                        하나하나 찾아가 부탁하는 설문 멈춰!<br />
+                        나도 하고 너도 하고 우리 함께 "Sulf"
+                    </p>
+                </div>
+            </figure>
+
+            <nav>
+                <div className="inner">
+                    <ul id="gnb">
+                        <li><Link to="#">사이트 안내</Link></li>
+                        <li className="border"></li>
+                        <li><Link to="#">분류</Link></li>
+                        <li className="border"></li>
+                        <li><Link to="#">설문 등록</Link></li>
+                        <li className="border"></li>
+                        <li><Link to="#">마이페이지</Link></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <section>
+                <div className="inner">
+                    <div className="box">Top3</div>
+
+                    <div className="wrapper">
+                        <div className="item">
+                            <div className="title">설문제목</div>
+                        </div>
+                        <div className="item">
+                            <div className="title">설문제목</div>
+                        </div>
+                        <div className="item">
+                            <div className="title">설문제목</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default Main

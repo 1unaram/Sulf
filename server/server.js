9c1ead4e9ffad33cebdbf5e1515a079394725b29
1unara <haram620@icloud.com>
2021-12-27 23:06:21 +0900

    merge 후 package.json 수정 및 server.js 수정

--- server/server.js
+++ server/server.js
@@ -14,9 +14,6 @@ app.use(cors());
 const test2 = require("./Router/postTest");
 app.use("/sendPost", test2);
 
-const id = require("./Router/id");
-app.use("/idReq", id);
-
 // 8080번 포트넘버를 가진 서버 생성
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 서버 실행 중 ..')
